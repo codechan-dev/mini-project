85b482b49aced61370e6fdff5feb94add731abfb
codechan-dev <officialchandru87@gmail.com>
2023-05-23 07:40:55 +0530

    master

---
+++ index.js
@@ -0,0 +1,45 @@
+//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
+
+const { Configuration, OpenAIApi } = require("openai");
+const express =require('express')
+const bodyparser = require(`body-parser`)
+const cors = require(`cors`)
+const configuration = new Configuration({
+    organization: "org-Bf4So4F6CNXaMoKejw2qigRu",
+    apiKey:"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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+
+
+const app =express()
+app.use(express.json())
+app.use(bodyparser.json())
+app.use(cors())
+
+const port=3080
+
+
+app.post('/', async (req,res)=>{
+  const { message }= req.body;
+  console.log("message:",message);
+  
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: `${message}`,
+  max_tokens: 500,
+  temperature: 0.7,
+});
+console.log(response.data.choices[0].text)
+res.json(
+  {
+    message:response.data.choices[0].text,
+  }
+)
+
+});
+
+app.listen(port,()=>{
+  console.log(`https://localhost:${port}`)
+})
---
+++ react project/demo/src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import React, { useState } from 'react';
+// import { json } from 'body-parser';
+// import { response } from 'express';
+
+function App() {
+
+  const [input,setInput]=useState("");
+  const [chatlog,setChatLog]=useState([
+  ]);
+
+  function claearchat(){
+    setChatLog([]);
+  }
+
+   async function handlesubmit(e) {
+    e.preventDefault()
+    let ChatLognew=[...chatlog,{ user:"me",message:`${input}`}];
+     setInput("");
+    //  setChatLog([ChatLognew])
+    const messages=ChatLognew.map((message)=>message.message).join("                         ");
+    const response = await fetch("http://localhost:3080/",
+    {
+      method:"POST",
+      headers:{
+        "content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        message:messages
+      })
+
+    }
+    );
+    const data= await response.json();
+    setChatLog([...ChatLognew,{ user:"gpt",message:`${data.message}`}])
+    console.log(data.message);
+  }
+  return (
+    <div className="App">
+    <aside className="sidemenu" >
+      <div className='newbutton' onClick={claearchat}> + new chat</div>
+    </aside>
+    <section className="mainmenu">
+      <div className='chat-log'>
+        {
+          chatlog.map((message,index)=>(
+            <ChatMessage key={index} message={message}/>
+
+          ))
+        }
+      
+        
+      </div>
+    <div className='chat-holder'>
+      <form onSubmit={handlesubmit}>
+      <input required type="text" value={input} onChange={(e)=> setInput(e.target.value)}  row="1" className='chat-input' placeholder='enter ur peoblem'></input>
+      </form>
+    
+    </div>
+    </section>
+    </div>
+  );
+}
+
+const ChatMessage=({message})=>{
+  return(
+    <div className={`chat-message ${message.user ==="gpt" && "chatgpt"}`}>
+    {/* <div className={`avathar ${message.user ==="gpt" && "chatgpt"}`}>
+
+    </div> */}
+    <div className='message'>
+      {message.message}
+
+    </div>
+
+  </div>
+  )
+}
+
+export default App;
